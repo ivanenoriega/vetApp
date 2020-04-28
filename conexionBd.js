f6d27c1d3ac872f67a0c679e373145602928ce4a
ricardo <ricardogenaizir1@gmail.com>
2020-04-27 21:43:22 -0300

    todo terminado!

--- conexionBd.js
+++ conexionBd.js
@@ -4,7 +4,7 @@ var conexion = mysql.createConnection({
   host: "localhost",
   port: "3306",
   user: "root",
-  password: "AsD21158651!",
+ // password: "AsD21158651!",
   database: "vetapp"
 });
 
